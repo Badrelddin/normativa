88dd92bea31c7b02fb69d0269694eb4404f2031d
badr <badr@badrweb.es>
2023-04-28 22:18:42 +0200

    reconfig-login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,7 +19,6 @@
   class="bg-image d-flex justify-content-center align-items-center"
   style="background-image: url('https://img.freepik.com/free-vector/gradient-grainy-texture_23-2148976749.jpg?w=1380&t=st=1682630611~exp=1682631211~hmac=491fc4a0588bd3cd6594a98bd7d62c314703eca45b63e758eabfbd517bf5b375');
         height: 100vh;">
-
     <div class="container py-4">
       <div class="row align-items-center">
         <div class="col-lg-6 mb-5 mb-lg-0">
@@ -28,24 +27,9 @@
               backdrop-filter: blur(30px);
               ">
             <div class="card-body p-5 shadow-5 text-center">
-              <h2 class="fw-bold mb-5">Registrarse ahora</h2>
+              <h2 class="fw-bold mb-5">Iniciar sesión</h2>
               <img src="a.png" class="w-30 rounded-4 shadow-4">
               <form>
-                <!-- 2 column grid layout with text inputs for the first and last names -->
-                <div class="row">
-                  <div class="col-md-6 mb-4">
-                    <div class="form-outline">
-                      <input type="text" id="form3Example1" class="form-control" />
-                      <label class="form-label" for="form3Example1">Nombre</label>
-                    </div>
-                  </div>
-                  <div class="col-md-6 mb-4">
-                    <div class="form-outline">
-                      <input type="text" id="form3Example2" class="form-control" />
-                      <label class="form-label" for="form3Example2">Apellidos</label>
-                    </div>
-                  </div>
-                </div>
                 <!-- Email input -->
                 <div class="form-outline mb-4">
                   <input type="email" id="form3Example3" class="form-control" />
@@ -60,7 +44,7 @@
     
                 <!-- Submit button -->
                 <button type="submit" class="btn btn-primary btn-block mb-4">
-                  Registrarse
+                  Iniciar sesión
                 </button>
               </form>
             </div>
